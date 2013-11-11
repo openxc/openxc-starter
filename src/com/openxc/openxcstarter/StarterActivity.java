d1685ac179e6941927824eef2cb040d0151164d5
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-11-11 22:29:09 +1100

    Simplify example, focusing on the OpenXC bits not Android lifecycle.

--- src/com/openxc/openxcstarter/StarterActivity.java
+++ src/com/openxc/openxcstarter/StarterActivity.java
@@ -21,15 +21,79 @@ public class StarterActivity extends Activity {
 
     private VehicleManager mVehicleManager;
     private TextView mEngineSpeedView;
-    private Intent mVmIntent = null;
-    private boolean mBound = false; // added for better example of Activity/Service lifecycle management.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter);
+        // grab a reference to the engine speed text object in the UI, so we can
+        // manipulate its value later from Java code
+        mEngineSpeedView = (TextView) findViewById(R.id.vehicle_speed);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // When the activity goes into the background or exits, we want to make
+        // sure to unbind from the service to avoid leaking memory
+        if(mVehicleManager != null) {
+            Log.i("openxc", "Unbinding from Vehicle Manager");
+            unbindService(mConnection);
+            mVehicleManager = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // When the activity starts up or returns from the background,
+        // re-connect to the VehicleManager so we can receive updates.
+        if(mVehicleManager == null) {
+            Intent intent = new Intent(this, VehicleManager.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    /* This is an OpenXC measurement listener object - the type is recognized
+     * by the VehicleManager as something that can receive measurement updates.
+     * Later in the file, we'll ask the VehicleManager to call the receive()
+     * function here whenever a new EngineSpeed value arrives.
+     */
+    EngineSpeed.Listener mSpeedListener = new EngineSpeed.Listener() {
+        public void receive(Measurement measurement) {
+            // When we receive a new EngineSpeed value from the car, we want to
+            // update the UI to display the new value. First we cast the generic
+            // Measurement back to the type we know it to be, an EngineSpeed.
+            final EngineSpeed speed = (EngineSpeed) measurement;
+            // In order to modify the UI, we have to make sure the code is
+            // running on the "UI thread" - Google around for this, it's an
+            // important concept in Android.
+            StarterActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    // Finally, we've got a new value and we're running on the
+                    // UI thread - we set the text of the EngineSpeed view to
+                    // the latest value
+                    mEngineSpeedView.setText("Vehicle speed (km/h): "
+                            + speed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
     private ServiceConnection mConnection = new ServiceConnection() {
         // Called when the connection with the VehicleManager service is established, i.e. bound.
         public void onServiceConnected(ComponentName className, IBinder service) {
             Log.i("openxc", "Bound to VehicleManager");
+            // When the VehicleManager starts up, we store a reference to it
+            // here in "mVehicleManager" so we can call functions on it
+            // elsewhere in our code.
             mVehicleManager = ((VehicleManager.VehicleBinder) service)
                     .getService();
 
+            // We want to receive updates whenever the EngineSpeed changes. We
+            // have an EngineSpeed.Listener (see above, mSpeedListener) and here
+            // we request that the VehicleManager call its receive() method
+            // whenever the EngineSpeed changes
             try {
                 mVehicleManager.addListener(EngineSpeed.class, mSpeedListener);
             } catch (VehicleServiceException e) {
@@ -43,37 +107,8 @@ public class StarterActivity extends Activity {
         public void onServiceDisconnected(ComponentName className) {
             Log.w("openxc", "VehicleManager Service  disconnected unexpectedly");
             mVehicleManager = null;
-            mBound = false;
         }
     };
-
-    @Override
-    /**
-     * As usual for onCreate, we set up the (static) UI here.
-     */
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_starter);
-        mEngineSpeedView = (TextView) findViewById(R.id.vehicle_speed);
-    }
-
-    @Override
-    /**
-     * We bind the service here and set the boolean rather than rely on
-     * onCreate, which gets called only when the whole Activity is created.
-     * This was done for resolving  issue #1 on Github in openxc-starter project
-     *
-     * Depending on how we got to onStart, the service might still be running, so we check mBound.
-     */
-    protected void onStart() {
-        super.onStart();
-        if (!mBound) {
-            mVmIntent = new Intent(this, VehicleManager.class);
-            mBound = bindService(mVmIntent, mConnection, Context.BIND_AUTO_CREATE);
-            if (!mBound)
-                Log.e("openxc", "Failed to bind to VehicleManager");
-        } // if we find it still bound, we do nothing.
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,47 +116,4 @@ public class StarterActivity extends Activity {
         getMenuInflater().inflate(R.menu.starter, menu);
         return true;
     }
-
-    /**
-     * We (conditionally) unbind the service here since in API level 8, we are
-     * not guaranteed to get to onStop() under conditions of
-     * "extreme memory pressure".
-     */
-    public void onPause() {
-        super.onPause();
-        if (mBound) {
-            Log.i("openxc", "Unbinding from Vehicle Manager");
-            unbindService(mConnection); // requires mConnection != null, which is true; it can't be null.
-            mBound = false; // unbindService returns void, so we assume it worked.
-        }
-    }
-
-    /**
-     * It is rare that onResume() is called after onPause() w/o intervening
-     * onStart(), but it can happen, so we reconnect to the service
-     * conditionally here, too.
-     */
-    public void onResume() {
-        super.onResume();
-        if (!mBound) {
-            mVmIntent = new Intent(this, VehicleManager.class);
-            mBound = bindService(mVmIntent, mConnection, Context.BIND_AUTO_CREATE);
-            if (mBound)
-                Log.i("openxc", "Binding to Vehicle Manager");
-            else
-                Log.e("openxc", "Failed to bind to Vehicle Manager");
-        }
-    }
-
-    EngineSpeed.Listener mSpeedListener = new EngineSpeed.Listener() {
-        public void receive(Measurement measurement) {
-            final EngineSpeed speed = (EngineSpeed) measurement;
-            StarterActivity.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    mEngineSpeedView.setText("Vehicle speed (km/h): "
-                            + speed.getValue().doubleValue());
-                }
-            });
-        }
-    };
 }
